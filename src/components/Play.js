5900d9520d34dc5ed309cd582164232eb57f53dd
Ashley Gyngell <ashleygyngell@gmail.com>
2022-04-23 01:02:55 +0100

    added genius scraper and api

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,11 +1,62 @@
 import React from 'react';
+import { getLyrics, getSong } from 'genius-lyrics-api';
+import { getKendrick } from '../lib/geniusapi';
+
+const options = {
+  apiKey: '4wX_AIcVI8fQHIbkWY8z95hKj_23o_04j8FOVD79b-1g_m2GXuYzyfC7pHRDoacU',
+  title: 'Blinding Lights',
+  artist: 'The Weeknd',
+  album: 'After Hours',
+  optimizeQuery: true,
+};
+
+function click() {
+  getLyrics(options).then((lyrics) => console.log(lyrics));
+  getSong(options).then((song) =>
+    console.log(`
+	${song.id}
+	${song.title}
+	${song.url}
+	${song.albumArt}
+	${song.lyrics}`)
+  );
+}
+
+// const KendrickStats = () => {};
+// setInterval(getLyrics(), 5000);
 
 const Home = () => {
+  const [kendrikinfo, setkendrikinfo] = React.useState(null);
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await getKendrick();
+        setkendrikinfo(data.response.hits);
+        console.log(data.response.hits);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getData();
+  }, []);
   return (
     <section className="hero is-fullheight-with-navbar is-success">
       <div className="hero-body">
         <div className="container">
           <p className="title has-text-centered">PLAY</p>
+          <button onClick={click}>Click for Song Lyrics</button>
+          <div>
+            {!kendrikinfo ? (
+              <div className="pageloader ">
+                <span className="title is-active ">Loading...</span>
+              </div>
+            ) : (
+              console.log(
+                'TEST',
+                kendrikinfo.map((x) => x.result.full_title)
+              )
+            )}
+          </div>
         </div>
       </div>
     </section>
--- src/components/test.js
+++ src/components/test.js
@@ -1,22 +1,22 @@
-// import React from 'react';
-// import { getLyrics, getSong } from 'genius-lyrics-api';
+import React from 'react';
+import { getLyrics, getSong } from 'genius-lyrics-api';
 
-// const options = {
-//   apiKey: '4wX_AIcVI8fQHIbkWY8z95hKj_23o_04j8FOVD79b-1g_m2GXuYzyfC7pHRDoacU',
-//   title: 'Blinding Lights',
-//   artist: 'The Weeknd',
-//   optimizeQuery: true,
-// };
+const options = {
+  apiKey: '4wX_AIcVI8fQHIbkWY8z95hKj_23o_04j8FOVD79b-1g_m2GXuYzyfC7pHRDoacU',
+  title: 'Blinding Lights',
+  artist: 'The Weeknd',
+  optimizeQuery: true,
+};
 
-// getLyrics(options).then((lyrics) => console.log(lyrics));
+getLyrics(options).then((lyrics) => console.log(lyrics));
 
-// getSong(options).then((song) =>
-//   console.log(`
-// 	${song.id}
-// 	${song.title}
-// 	${song.url}
-// 	${song.albumArt}
-// 	${song.lyrics}`)
-// );
+getSong(options).then((song) =>
+  console.log(`
+	${song.id}
+	${song.title}
+	${song.url}
+	${song.albumArt}
+	${song.lyrics}`)
+);
 
-// setInterval(getLyrics(), 5000);
+setInterval(getLyrics(), 5000);
