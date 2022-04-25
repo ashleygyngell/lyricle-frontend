8f6f33b61019e9102802f409d755e6dd19ca2413
Ashley Gyngell <ashleygyngell@gmail.com>
2022-04-25 18:05:17 +0100

    url update

--- src/lib/geniusapi.js
+++ src/lib/geniusapi.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 
 export const getKendrick = () => {
-  return axios.get(
-    `${process.env.REACT_APP_API_SERVER}api.genius.com/search?q=KendrickLamar`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.client_access_token}`,
-      },
-    }
-  );
+  return axios.get('http://api.genius.com/search?q=KendrickLamar', {
+    headers: {
+      Authorization: `Bearer ${process.env.client_access_token}`,
+    },
+  });
 };
