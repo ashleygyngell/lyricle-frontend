e6236a8e9fb1197e81b882088780148cba05521e
Ashley Gyngell <ashleygyngell@gmail.com>
2022-12-22 12:34:29 +0000

    cors attempt

--- src/lib/geniusapi.js
+++ src/lib/geniusapi.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const getKendrick = (searchForArtist) => {
   return axios.get(
-    `http://api.genius.com/search?q="${searchForArtist}"&access_token=${process.env.client_access_token}`
+    `https://api.genius.com/search?q="${searchForArtist}"&access_token=${process.env.client_access_token}`
   );
 };
